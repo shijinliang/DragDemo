023de7154b0a7dbc9c4eafe6997212dedc3cd471
xiaoshi <sjl_leaf@163.com>
2017-09-12 11:44:44 +0800

    Initial Commit

---
+++ huadong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  huadong
+//
+//  Created by xiaoshi on 2017/9/12.
+//  Copyright © 2017年 ClouderWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ huadong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  huadong
+//
+//  Created by xiaoshi on 2017/9/12.
+//  Copyright © 2017年 ClouderWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
